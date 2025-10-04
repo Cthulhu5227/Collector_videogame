5daf8585b485391877cbae8c667008cc1ac7525d
User <cthulhu5227>
2025-10-04 16:11:02 -0700

    basic level menu

--- level_menu.gd
+++ level_menu.gd
@@ -1,5 +1,24 @@
 extends Control
+var number_of_levels = 3
+@onready var level_button = $level_button
+@onready var level_container = $level_container
 
 func _ready():
-	var child = $level_button
-	for level in $res://levels/
+	for level in range(number_of_levels):
+		var level_scene = load_level(level+1)
+		var clone = level_button.duplicate()
+		var function_access = func load_level(): get_tree().change_scene_to_file(level_scene)
+		clone.connect("pressed", function_access)
+		clone.visible = true
+		level_container.add_child(clone)
+		
+
+
+func load_level(level_number: int):
+	var level_path = "res://levels/level_%d.tscn" % level_number
+	var level_scene = load(level_path)
+	if level_scene:
+		print("Loaded:", level_path)
+	else:
+		print("Level not found:", level_path)
+	return level_path
