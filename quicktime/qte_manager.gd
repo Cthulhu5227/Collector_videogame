e33d1b562fd7860f7610809f25ec93f39dca67e4
Merrick McLean <125709203+Merrick-McLean@users.noreply.github.com>
2025-10-04 11:18:55 -0700

    adds qte basic

---
+++ quicktime/qte_manager.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+var qte_type: String # "catch", "match", "mash", "press"
+var qte_diff: float # difficulty
+var qte_input: Array # []
+
+# do something with these
+var qte_active := false
+var qte_success := false
+
+func _ready():	
+	# practise input
+	qte_type = "press"
+	qte_diff = 3
+	qte_input = ["T", "E", "E", "T", "H"]
+	
+	"""
+	qte_type = "mash"
+	qte_diff = 0.02
+	qte_input = ["SPACE"]
+	"""
+	
+	"""
+	qte_type = "match"
+	qte_diff = 6
+	qte_input = ["U", "D", "D", "L", "R"]
+	"""
+	
+	match qte_type:
+		"catch":
+			call_other_script("res://quicktime/qte_catch.gd", qte_diff, qte_input,[])
+		"match":
+			call_other_script("res://quicktime/qte_match.gd", qte_diff, qte_input, [$Timer, $Timer_Bar, $QTE_Instr_Label, $QTE_Input_Label])
+		"mash":
+			call_other_script("res://quicktime/qte_mash.gd", qte_diff, qte_input, [$Timer_Bar, $QTE_Instr_Label, $QTE_Input_Label])
+		"press":
+			call_other_script("res://quicktime/qte_press.gd", qte_diff, qte_input, [$Timer, $Timer_Bar, $QTE_Instr_Label, $QTE_Input_Label])
+		_:
+			print("Warning:", qte_type, "is not an accepted QTE")
+
+func call_other_script(path: String, difficulty: float, input: Array, refs: Array):
+	var qte_inst = load(path).new()
+	add_child(qte_inst)
+	if qte_inst.has_method("initialize"):
+		qte_inst.initialize(difficulty, input, refs)
+	else:
+		print("Warning: Script", path, "has no 'initialize' method")
---
+++ quicktime/qte_mash.gd
@@ -0,0 +1,47 @@
+extends Node
+
+var qte_instance_active := false
+
+var timer
+var mash_bar
+var instruction_label
+var input_label
+
+# timer bar stuff
+var dec_speed := 0.0
+var input_value := 0.0
+
+# input key
+var key_name
+
+func initialize(difficulty: float, input: Array, node_refs):	
+	# get qte elements
+	mash_bar = node_refs[0]
+	instruction_label = node_refs[1]
+	input_label = node_refs[2]
+	
+	# set up mash bar
+	mash_bar.scale.x = 0
+	dec_speed = difficulty * 0.01
+	input_value = difficulty
+	
+	# Start QTE
+	instruction_label.bbcode_text = "[center][color=white]Mash![/color][/center]"
+	qte_instance_active = true
+	
+	key_name = input[0]
+	input_label.bbcode_text = "[center][color=white]%s[/color][/center]" % key_name
+	
+func _process(_delta):
+	if qte_instance_active:
+		if mash_bar.scale.x >= 1:
+			mash_bar.scale.x = 1
+			qte_instance_active = false
+			input_label.bbcode_text = "[center][color=white]YAY[/color][/center]"
+		
+		mash_bar.scale.x -= dec_speed
+
+func _input(event):
+	var key_value := OS.find_keycode_from_string(key_name)
+	if (qte_instance_active) and (event is InputEventKey) and (event.pressed) and (event.keycode == key_value): 
+		mash_bar.scale.x += input_value
