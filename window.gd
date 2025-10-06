95d3f97d334c083168c5087c284fe24f6016b6fd
cthulhu5227 <jaspurnjuno74@gmail.com>
2025-10-06 09:36:26 -0700

    fixing exit ui

--- window.gd
+++ window.gd
@@ -1,19 +1,28 @@
 extends Node2D
-@onready var tutorial = $Label
+var useable = false
+@onready var label = $Label
 
 func _ready():
-	tutorial.visible = false
+	useable = false
+	label.rotation =  -rotation
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player") : 
-		tutorial.visible = true
+		label.visible = true
+		useable = true
+		if !has_all_teeth():
+			label.text = "Get more teeth!"
+		else:
+			label.text = "Press E to Leave"
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player") : 
-		tutorial.visible = false
+		label.visible = false
+		useable = false
 		
 func has_all_teeth():
 	return get_tree().get_nodes_in_group("bed").size() <= Inventory.teeth
 		
 
 func _input(event: InputEvent):
-	if tutorial.visible and event.is_action_pressed("interact") and has_all_teeth():
+	if useable and event.is_action_pressed("interact") and has_all_teeth():
 		Ui.end_level()
